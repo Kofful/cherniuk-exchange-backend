1b3af4bd8d1fd53467f39b3626987b1da29a7148
Vladyslav Cherniuk <vladyslav.cherniuk@nixsolutions.com>
2021-09-30 15:08:25 +0300

    Created migration for unique fields in users table, changed magic numbers in migration to user constants

--- migrations/Version20210930082309.php
+++ migrations/Version20210930082309.php
@@ -6,12 +6,11 @@ namespace DoctrineMigrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
-use App\Entity\User;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210929145844 extends AbstractMigration
+final class Version20210930082309 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,11 +19,13 @@ final class Version20210929145844 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE users CHANGE role_id role_id INT DEFAULT ' . User::DEFAULT_ROLE_ID . ' NOT NULL, CHANGE status_id status_id INT DEFAULT ' . User::DEFAULT_STATUS_ID . ' NOT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9F85E0677 ON users (username)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_1483A5E9E7927C74 ON users (email)');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE users CHANGE role_id role_id INT NOT NULL, CHANGE status_id status_id INT NOT NULL');
+        $this->addSql('DROP INDEX UNIQ_1483A5E9F85E0677 ON users');
+        $this->addSql('DROP INDEX UNIQ_1483A5E9E7927C74 ON users');
     }
 }
