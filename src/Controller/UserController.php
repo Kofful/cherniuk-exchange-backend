6851044b7626121095dfb701595dffa597708a59
Vladyslav Cherniuk <vladyslav.cherniuk@nixsolutions.com>
2021-11-19 17:41:21 +0200

    Created sticker drop route with getting sticker randomly and pushing it to user inventory

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,10 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Service\Inventory\InventoryService;
+use App\Service\Sticker\StickerService;
 use App\Service\User\UserService;
+use DateTime;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class UserController extends AbstractController
 {
@@ -18,11 +22,29 @@ class UserController extends AbstractController
         ]);
     }
 
-    public function dropSticker(Request $request): Response
+    public function dropSticker(
+        StickerService $stickerService,
+        UserService $userService,
+        InventoryService $inventoryService,
+        TranslatorInterface $translator
+    ): Response
     {
-        $sticker = "test";
-        return $this->json([
-            "sticker" => $sticker
-        ]);
+        $response = [];
+        $status = 200;
+
+        $user = $this->getUser();
+
+        $sticker = $stickerService->dropSticker($user);
+
+        if (isset($sticker)) {
+            $response = $sticker;
+            $inventoryService->addItem($user, $sticker);
+            $userService->updateRewardedAt($user);
+        } else {
+            $response = [$translator->trans("sticker.cannot.receive", [], "responses")];
+            $status = 403;
+        }
+
+        return $this->json($response, $status);
     }
 }
