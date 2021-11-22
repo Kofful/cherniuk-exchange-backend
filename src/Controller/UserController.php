6614a9be187b713dd97e53fdc3a2f2d23742e13a
Vladyslav Cherniuk <vladyslav.cherniuk@nixsolutions.com>
2021-11-22 17:18:32 +0200

    Created endpoint in UserController for getting user info

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use App\Service\StatusCode;
 use App\Service\User\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use const Grpc\STATUS_OK;
 
 class UserController extends AbstractController
 {
@@ -14,5 +19,26 @@ class UserController extends AbstractController
         return $this->json([
             "user" => $user
         ]);
+    }
+
+    public function getUserInfo(UserRepository $userRepository, TranslatorInterface $translator, Request $request): Response
+    {
+        $response = [];
+        $status = StatusCode::STATUS_OK;
+
+        $userId = $request->attributes->get("id");
+        $user = $userRepository->find($userId);
+
+        if(isset($user)) {
+            $response = [
+                "id" => $user->getId(),
+                "username" => $user->getUsername()
+            ];
+        } else {
+            $status = StatusCode::STATUS_BAD_REQUEST;
+            $response = [$translator->trans("user.not.found", [], "responses")];
+        }
+
+        return $this->json($response, $status);
     }
 }
