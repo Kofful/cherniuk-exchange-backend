80a91b996dc316bea7c014edc19af0b0b97ec6db
Vladyslav Cherniuk <vladyslav.cherniuk@nixsolutions.com>
2021-12-16 16:51:52 +0200

    Added validation in offer creating endpoint

---
+++ src/Service/Offer/OfferService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Service\Offer;
+
+use App\Entity\Offer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\NameConverter\CamelCaseToSnakeCaseNameConverter;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
+class OfferService
+{
+
+}
